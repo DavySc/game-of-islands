85b67070d4b0921cafe1e206e4b969cb86791bfc
Davy <d.vdschoot@gmail.com>
2024-08-16 16:21:16 +0200

    Started real testing

--- test/islands_engine_test.exs
+++ test/islands_engine_test.exs
@@ -5,4 +5,8 @@ defmodule IslandsEngineTest do
   test "greets the world" do
     assert IslandsEngine.hello() == :world
   end
+
+  test "out-of-bounds coordinate fails" do
+    assert IslandsEngine.Coordinate.new(11, 1) == {:error, :invalid_coordinate}
+  end
 end
