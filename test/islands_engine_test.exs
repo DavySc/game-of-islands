9f4cf8e091e6a34882cc556db06a00a309fb95a8
Davy <d.vdschoot@gmail.com>
2024-08-16 16:36:30 +0200

    Testing l_shapes

--- test/islands_engine_test.exs
+++ test/islands_engine_test.exs
@@ -9,4 +9,23 @@ defmodule IslandsEngineTest do
   test "out-of-bounds coordinate fails" do
     assert IslandsEngine.Coordinate.new(11, 1) == {:error, :invalid_coordinate}
   end
+
+  test "l_shape island can be placed at (4,6)" do
+    {:ok, coordinate} = IslandsEngine.Coordinate.new(4, 6)
+
+    desired_island =
+      {:ok,
+       %IslandsEngine.Island{
+         coordinates:
+           MapSet.new([
+             %IslandsEngine.Coordinate{row: 4, col: 6},
+             %IslandsEngine.Coordinate{row: 5, col: 6},
+             %IslandsEngine.Coordinate{row: 6, col: 6},
+             %IslandsEngine.Coordinate{row: 6, col: 7}
+           ]),
+         hit_coordinates: MapSet.new([])
+       }}
+
+    assert IslandsEngine.Island.new(:l_shape, coordinate) = desired_island
+  end
 end
